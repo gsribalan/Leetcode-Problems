496226ec28ccc5cafb8d33126a0c1e3b0858ffd4
Sri Balan Ganesan <gsribalan11@gmail.com>
2026-08-20 10:35:49 +0530

    Time: 2 ms (87.08%), Space: 49.1 MB (72.19%) - LeetHub

---
+++ 0143-reorder-list/0143-reorder-list.java
@@ -0,0 +1,30 @@
+class Solution {
+    public void reorderList(ListNode head) {
+        if (head == null || head.next == null) return;
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode second = slow.next;
+        slow.next = null;
+        ListNode prev = null;
+        while (second != null) {
+            ListNode nextTemp = second.next;
+            second.next = prev;
+            prev = second;
+            second = nextTemp;
+        }
+        ListNode first = head;
+        second = prev; 
+        while (second != null) {
+            ListNode tmp1 = first.next;
+            ListNode tmp2 = second.next;
+            first.next = second;
+            second.next = tmp1;
+            first = tmp1;
+            second = tmp2;
+        }
+    }
+}
